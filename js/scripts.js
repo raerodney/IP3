aa1c6749e22fc026daf9272d51f64d5fc7916828
raerodney <rodneykinyua@outlook.com>
2021-06-22 11:35:45 +0300

    deleted mailchmp due to too much bugs

--- js/scripts.js
+++ js/scripts.js
@@ -90,7 +90,7 @@ $(document).ready(function(){
       var email = $("input#MERGE0").val();
       var message = $("textarea#comment").val();
       if ($("input#MERGE1").val() && $("input#MERGE0").val()){
-        alert (name + ", we have received your message. Thank you for reaching out to us.");
+        alert (name + ",Thanks for reaching out to rodney & co");
       }
       else {
         alert("Please enter your name and email!");
